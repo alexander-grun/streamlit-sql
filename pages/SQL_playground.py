1fcde5d2284f6b04245d9efed559f3453e6f1b15
alex <vintego92@gmail.com>
2023-05-19 23:30:36 +0300

    injection protection

--- pages/SQL_playground.py
+++ pages/SQL_playground.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 from PIL import Image
+import re
 
 st.set_page_config(layout="wide")
 hide_st_style = """
@@ -38,6 +39,23 @@ def sql_executor(raw_code):
 	data = cursor.fetchall()
 	return data
 
+def is_valid_query(user_query):
+    # Define the list of keywords to filter
+    keywords_to_filter = ['DROP', 'ALTER', 'CREATE', 'UPDATE', 'DELETE', 'EXECUTE', 'EXEC']
+
+    # Convert user query to uppercase for case-insensitive matching
+    user_query = user_query.upper()
+
+    # Use regular expressions to check for the presence of keywords
+    pattern = r'\b(?:{})\b'.format('|'.join(map(re.escape, keywords_to_filter)))
+    match = re.search(pattern, user_query)
+
+    if match:
+        return False  # Invalid query containing filtered keywords
+    else:
+        return True  # Valid query
+
+
 st.subheader("SQL Playground")
 
 col1, col2 = st.columns([1,2])
@@ -46,26 +64,29 @@ with col1:
     with st.form(key='query_form'):
         raw_code = st.text_area("SQL Code Here", value= 'select * from jobs')
         submit_code = st.form_submit_button("Execute")
+        if is_valid_query(raw_code) == False:
+            st.warning('Query contains forbidden keywords and is not allowed. Do not try to modify the database', icon="⚠️")
+        else:
 
-    # Table of Info
+            # Table of Info
 
-    with st.expander("Tables Info", expanded=True):
-        table_info = column_names_dict
+            with st.expander("Tables Info", expanded=True):
+                table_info = column_names_dict
 
-        # table_info = {'city': city, 'country': country, 'countrylanguage': countrylanguage}
-        st.json(table_info)
+                # table_info = {'city': city, 'country': country, 'countrylanguage': countrylanguage}
+                st.json(table_info)
 
-    # Results Layouts
-    with col2:
-        if submit_code:
-            st.info("Query Submitted")
-            st.code(raw_code)
+            # Results Layouts
+            with col2:
+                if submit_code:
+                    st.info("Query Submitted")
+                    st.code(raw_code)
 
-            # Results
-            query_results = sql_executor(raw_code)
-            # with st.expander("Results"):
-            #     st.write(query_results)
+                    # Results
+                    query_results = sql_executor(raw_code)
+                    # with st.expander("Results"):
+                    #     st.write(query_results)
 
-            with st.expander("Results", expanded=True):
-                query_df = pd.DataFrame(query_results, columns=[description[0] for description in cursor.description])
-                st.dataframe(query_df)
+                    with st.expander("Results", expanded=True):
+                        query_df = pd.DataFrame(query_results, columns=[description[0] for description in cursor.description])
+                        st.dataframe(query_df)
