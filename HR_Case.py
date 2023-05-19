aa3fed661073630e2ade8ba025ce462e1c69865d
alex <vintego92@gmail.com>
2023-05-19 23:16:36 +0300

    description

--- HR_Case.py
+++ HR_Case.py
@@ -50,13 +50,13 @@ def main():
     col1, col2 = st.columns([55, 45])
     with col1:
         st.text("Case description")
-        st.write("""Part 1:
+        st.write("""**Part 1:**
         Head of HR asked you to answer some of the questions for him while working with this data, provide answers in short form as if you are replying to the email. As a challenging task - try answering without looking into the big table, try building a query that would fetch you a result.
 
 1. Who is the manager with the most employees, how many, and which team is it? 
 2. Who is the manager with the highest average salary in his team excluding CEO and Management Team? What is the team and what’s the average salary?
 3. In which city should we open the third office? Currently, we have 2 in two of the most popular cities where our employees are.""")
-        st.write("""Part 2:
+        st.write("""**Part 2:**
                              The second part of the request from Head of HR is the to help creating the wide table for HR analyst to build dashboards on. You don’t need to build any dashboards, but your SQL skills are needed to join necessary tables and produce the end result.
                               To help you with specifications HR team made a prototype table""")
 
