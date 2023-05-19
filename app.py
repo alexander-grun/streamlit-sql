8fb72832e8b6f02db638467682479e465f6ad1aa
alex <vintego92@gmail.com>
2023-05-19 14:57:01 +0300

    logo and github

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from PIL import Image
 st.set_page_config(layout="wide")
 hide_st_style = """
             <style>
+            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
